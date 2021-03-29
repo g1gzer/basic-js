f114bf72507efa59aecd0e823ad7635bcb4aca72
g1gzer <38072226+g1gzer@users.noreply.github.com>
2021-03-29 19:40:20 +0300

    init: task completed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(expression) {
+        this.expression = expression;
+    }
+
+    encrypt(message, key) {
+        if (message == null || key == null) {
+            throw Error();
+        };
+        const firstCode = 'A'.charCodeAt();
+        const latinAlphabet = /[A-Za-z]/;
+        const alphabetLength = 26;
+        let count = 0;
+
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase();
+
+        message.forEach((el, index) => {
+            if (latinAlphabet.test(el)) {
+                message[index] = String.fromCharCode(
+                    ((el.charCodeAt() - firstCode
+                        + (key[count % key.length].charCodeAt() - firstCode))
+                        % alphabetLength)
+                    + firstCode);
+                count += 1;
+            }
+        });
+        return this.expression === false ? message.reverse().join('') : message.join('');
+    }
+
+    decrypt(message, key) {
+        if (message == null || key == null) {
+            throw Error();
+        }
+        const latinAlphabet = /[A-Za-z]/;
+        const alphabetLength = 26;
+        const firstCode = 'A'.charCodeAt();
+        let count = 0;
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase();
+
+        message.forEach((el, index) => {
+            if (latinAlphabet.test(el)) {
+                message[index] = String.fromCharCode(
+                    ((el.charCodeAt() - firstCode
+                        + (alphabetLength - (key[count % key.length].charCodeAt() - firstCode)))
+                        % alphabetLength)
+                    + firstCode);
+                count += 1;
+            }
+        });
+        return this.expression === false ? message.reverse().join('') : message.join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
